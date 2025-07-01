da37427914a19a49ef9a9ef1df9fc3de447e44a6
deekshaa7 <deekshaasharma7@gmail.com>
2025-07-01 17:11:52 +0530

    upload project

--- workvera_frontend/src/api.js
+++ workvera_frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Ensure your backend API URL is correctly set here or in an environment variable
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://127.0.0.1:8000/api'; 
+const API_BASE_URL = process.env.REACT_APP_API_URL || 'https://deekshaasharma7.pythonanywhere.com/api'; 
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
